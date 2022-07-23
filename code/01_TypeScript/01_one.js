b9cecfdfd00d79270925916c513949e9bff59d6d
李森 <345365156@qq.com>
2022-07-23 11:13:47 +0800

    剩余文件的提交

---
+++ code/01_TypeScript/01_one.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    function sayHi(str) {
+        return "你好，" + str;
+    }
+    let text = 'li';
+    console.log(sayHi(text));
+})();
---
+++ code/02_TS的自动编译/02_auto.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    function sayHi(str) {
+        return "你好，" + str;
+    }
+    let text = '李森';
+    console.log(sayHi(text));
+})();
